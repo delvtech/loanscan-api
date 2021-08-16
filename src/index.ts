7952d5bd2acbb8015b9dd2655638f80c639186b0
Danny Delott <dannydelott@gmail.com>
2021-08-16 08:19:57 -0700

    Inline the balancer vault address

--- src/index.ts
+++ src/index.ts
@@ -44,8 +44,9 @@ interface LoanScan {
   borrowRates: Rates[];
 }
 
-async function generateLoanScan(terms: string[]): Promise<string>{
+const BALANCER_VAULT_ADDRESS = "0xBA12222222228d8Ba445958a75a0704d566BF2C8";
 
+async function generateLoanScan(terms: string[]): Promise<string> {
   const [signer] = await ethers.getSigners();
 
   // get the official list of Element deployed addresses.
@@ -56,14 +57,13 @@ async function generateLoanScan(terms: string[]): Promise<string>{
   );
   let loanScan: LoanScan = {
     lendRates: [],
-    borrowRates: []
+    borrowRates: [],
   };
   for (const trancheListKey of terms) {
     const trancheList = deploymentAddresses.tranches[trancheListKey];
     for (const tranche of trancheList) {
       const ptPool = tranche.ptPool.address;
       const trancheAddress = tranche.address;
-      const balancerVaultAddress = deploymentAddresses.balancerVault;
 
       // get the symbols for the term address
       const termTokenSymbols: TermTokenSymbolsResult =
@@ -82,9 +82,11 @@ async function generateLoanScan(terms: string[]): Promise<string>{
       );
 
       const totalSupply = await getTotalSupply(ptPool, signer);
-      let reserves = await getReserves(ptPool, balancerVaultAddress, signer);
-      const ptIndex = reserves.tokens[0].toLowerCase() == base.toLowerCase() ? 1 : 0;
-      let baseIndex = reserves.tokens[0].toLowerCase() == base.toLowerCase() ? 0 : 1;
+      let reserves = await getReserves(ptPool, BALANCER_VAULT_ADDRESS, signer);
+      const ptIndex =
+        reserves.tokens[0].toLowerCase() == base.toLowerCase() ? 1 : 0;
+      let baseIndex =
+        reserves.tokens[0].toLowerCase() == base.toLowerCase() ? 0 : 1;
       const ptReserves = reserves.balances[ptIndex];
       let baseReserves = reserves.balances[baseIndex];
       const baseDecimals = reserves.decimals[baseIndex];
@@ -99,12 +101,12 @@ async function generateLoanScan(terms: string[]): Promise<string>{
         baseDecimals
       );
 
-      const fixedAPR = calcFixedAPR(ptSpotPrice, timeRemainingSeconds)/100;
+      const fixedAPR = calcFixedAPR(ptSpotPrice, timeRemainingSeconds) / 100;
       const rates: Rates = {
         apr: fixedAPR,
         apy: fixedAPR,
-        tokenSymbol: trancheListKey.toUpperCase()
-      }
+        tokenSymbol: trancheListKey.toUpperCase(),
+      };
 
       loanScan.lendRates.push(rates);
     }
@@ -112,47 +114,46 @@ async function generateLoanScan(terms: string[]): Promise<string>{
   return JSON.stringify(loanScan, null, 2);
 }
 
-async function updateBucket(data: string){
+async function updateBucket(data: string) {
   // Load the AWS SDK for Node.js
-  var AWS = require('aws-sdk');
-  // Set the region 
-  AWS.config.update({region: 'us-east-2'});
+  var AWS = require("aws-sdk");
+  // Set the region
+  AWS.config.update({ region: "us-east-2" });
   // Create S3 service object
-  var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
   // call S3 to retrieve upload file to specified bucket
-  var uploadParams = {Bucket: 'elementfi', Key: '', Body: ''};
-  var file = 'loanscan';
-
+  var uploadParams = { Bucket: "elementfi", Key: "", Body: "" };
+  var file = "loanscan";
 
   // Configure the file stream and obtain the upload parameters
-  var fs = require('fs');
+  var fs = require("fs");
   var fileStream = fs.createReadStream(file);
-  fileStream.on('error', function(err) {
-    console.log('File Error', err);
+  fileStream.on("error", function (err) {
+    console.log("File Error", err);
   });
   uploadParams.Body = fileStream;
-  var path = require('path');
+  var path = require("path");
   uploadParams.Key = path.basename(file);
 
   // call S3 to retrieve upload file to specified bucket
-  s3.upload (uploadParams, function (err, data) {
+  s3.upload(uploadParams, function (err, data) {
     if (err) {
       console.log("Error", err);
-    } if (data) {
+    }
+    if (data) {
       console.log("Upload Success", data.Location);
     }
   });
-
 }
 
-async function setBucketPolicy(){
-  var AWS = require('aws-sdk');
-  // Set the region 
-  AWS.config.update({region: 'us-east-2'});
+async function setBucketPolicy() {
+  var AWS = require("aws-sdk");
+  // Set the region
+  AWS.config.update({ region: "us-east-2" });
 
   // Create S3 service object
-  var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+  var s3 = new AWS.S3({ apiVersion: "2006-03-01" });
 
   var readOnlyAnonUserPolicy = {
     Version: "2012-10-17",
@@ -161,14 +162,10 @@ async function setBucketPolicy(){
         Sid: "AddPerm",
         Effect: "Allow",
         Principal: "*",
-        Action: [
-          "s3:GetObject"
-        ],
-        Resource: [
-          ""
-        ]
-      }
-    ]
+        Action: ["s3:GetObject"],
+        Resource: [""],
+      },
+    ],
   };
 
   // create selected bucket resource string for bucket policy
@@ -176,10 +173,13 @@ async function setBucketPolicy(){
   readOnlyAnonUserPolicy.Statement[0].Resource[0] = bucketResource;
 
   // convert policy JSON into string and assign into params
-  var bucketPolicyParams = {Bucket: 'elementfi', Policy: JSON.stringify(readOnlyAnonUserPolicy)};
+  var bucketPolicyParams = {
+    Bucket: "elementfi",
+    Policy: JSON.stringify(readOnlyAnonUserPolicy),
+  };
 
   // set the new policy on the selected bucket
-  s3.putBucketPolicy(bucketPolicyParams, function(err, data) {
+  s3.putBucketPolicy(bucketPolicyParams, function (err, data) {
     if (err) {
       // display error message
       console.log("Error", err);
@@ -189,11 +189,11 @@ async function setBucketPolicy(){
   });
 }
 
-async function main(){
+async function main() {
   const terms = ["dai"];
   const data: string = await generateLoanScan(terms);
   console.log(data);
-  fs.writeFileSync('loanscan', data,'utf8');
+  fs.writeFileSync("loanscan", data, "utf8");
   await updateBucket(data);
   await setBucketPolicy();
 }
